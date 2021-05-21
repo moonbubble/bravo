30a7585bf452d8fed3eb1145c93730b381406103
Ralphine <r.deroo99@gmail.com>
2021-05-21 11:49:16 +0200

    Initial Bravo and Next.js commit

---
+++ bravo-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a23de96aa52008b0e234ff10c348d593'),
+    },
+  },
+});
---
+++ bravo-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '89363aa0-c699-4ee9-a6c3-67c41b9cdacc'
+};
